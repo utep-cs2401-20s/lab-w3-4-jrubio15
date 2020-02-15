3b6020506bac52c1a7e4af49c8bf6ec5bbfeaf7c
Javier Rubio Jr <jrubio15@miners.utep.edu>
2020-02-14 19:13:31 -0700

    Made progress but NOT finished TorusGameOfLife.

--- code/src/TorusGameOfLife.java
+++ code/src/TorusGameOfLife.java
@@ -1,2 +1,8 @@
-public class TorusGameOfLife {
+public class TorusGameOfLife extends GameOfLife{
+    public TorusGameOfLife(int size){
+        super(size);
+    }
+    public TorusGameOfLife(int [][] board){
+        super(board);
+    }
 }
