bad4002f79997138768d7780fe7f557d4f30f8b0
Javier Rubio Jr <jrubio15@miners.utep.edu>
2020-02-15 15:35:37 -0700

    Test Cases

--- code/src/TorusGOLTester.java
+++ code/src/TorusGOLTester.java
@@ -24,7 +24,7 @@ class TorusGOLTester {
         assertArrayEquals(outcome, gol.getBoard());
     }
     @Test
-    public void testEvolution() {
+    public void testEvolution1() {
         int[][] A = {
                 {0,0,0},
                 {0,1,0},
@@ -38,6 +38,78 @@ class TorusGOLTester {
         assertArrayEquals(B,x.getBoard());
     }
 
+    @Test
+    public void testEvolution2(){
+        int[][] A =
+                {{0,0,1},
+                 {0,0,0},
+                 {0,0,0}};
+        int[][] B =
+                {{0,0,0},
+                 {0,0,0},
+                 {0,0,0}};
+        GameOfLife z = new GameOfLife(A);
+        z.evolution(1);
+        assertArrayEquals(B,z.getBoard());
+    }
+
+    @Test
+    public void testEvolution3(){
+        int [][] A =
+                {{0,0,0},
+                 {0,0,0},
+                 {1,0,1}};
+        int [][] B =
+                {{0,0,0},
+                 {0,0,0},
+                 {0,0,0}};
+        GameOfLife q = new GameOfLife(A);
+        q.evolution(1);
+        assertArrayEquals(B, q.getBoard());
+    }
+    @Test
+    public void testEvolution4(){
+        int [][] A =
+                {{1,0,1},
+                 {0,0,0},
+                 {1,0,1}};
+        int [][] B =
+                {{0,0,0},
+                 {0,0,0},
+                 {0,0,0}};
+        GameOfLife e = new GameOfLife(A);
+        e.evolution(1);
+        assertArrayEquals(B,e.getBoard());
+    }
+    @Test
+    public void testEvolution5(){
+        int [][] A =
+                {{1,0,1},
+                 {0,0,0},
+                 {0,0,0}};
+        int [][] B =
+                {{0,0,0},
+                 {0,0,0},
+                 {0,0,0}};
+        GameOfLife r = new GameOfLife(A);
+        r.evolution(1);
+        assertArrayEquals(B,r.getBoard());
+
+    }
+    @Test
+    public void testOneStep1(){
+        int [][] A =
+                        {{0,0,0,1},
+                         {0,0,0,0},
+                         {0,0,0,0}};
+        int [][] B =
+                        {{0,0,0},
+                        {0,0,0},
+                        {0,0,0}};
+        GameOfLife n = new GameOfLife(A);
+        n.evolution(1);
+        assertArrayEquals(B,n.getBoard());
+    }
 
     public static void main(String[] args) {
         int[][] board = {
